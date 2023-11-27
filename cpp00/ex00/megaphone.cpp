731616f7e5b132f8e6fb874a91b819d7f26f92cb
Thibault <thibault.nguyen26@gmail.com>
2023-11-27 17:03:31 +0100

    just for instead of while in megaphone

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: thibault <thibault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 03:38:10 by thibault          #+#    #+#             */
-/*   Updated: 2023/11/26 19:54:48 by thibault         ###   ########.fr       */
+/*   Updated: 2023/11/27 16:44:36 by thibault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,10 @@
 
 int main(int argc, char *argv[])
 {
-	int i = 0;
-
 	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	} else if (argc > 1) {
-		while (argv[++i]) {
+	} else {
+		for (int i = 1; i < argc; ++i) {
 			for (size_t j = 0; j < strlen(argv[i]); j++) {
 				std::cout << (char)std::toupper(argv[i][j]);
 			}
